1e1cd298c5a0249dad1067d6ef91091046d939c0
yy <1197779219@qq.com>
2017-11-30 17:17:19 +0800

    真的是大坑

--- src/main/webapp/scripts/supplierlist.js
+++ src/main/webapp/scripts/supplierlist.js
@@ -137,7 +137,7 @@ $(document).ready(function() {
     // debugger
     if (type == 'button' && operate == '删除') {
       $('#delete').on('click', function(){
-        const array = [data.companies[name].comid];
+        const array = [data[name].comid];
         console.log(array);
         $.ajax({
           url: "http://localhost:8888/company/deleteCompany",
