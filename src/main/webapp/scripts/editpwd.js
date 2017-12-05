4fafdc04a07d02c25ae7a344bbd9485e87531e2e
jyf <jyf@qq.com>
2017-12-05 13:42:22 +0800

    0

---
+++ src/main/webapp/scripts/editpwd.js
@@ -0,0 +1,6 @@
+$(function(){
+
+  var namestorage=sessionStorage.getItem("user_name");
+  document.getElementById("username").innerText=namestorage;
+
+})
--- src/main/webapp/scripts/userlist.js
+++ src/main/webapp/scripts/userlist.js
@@ -1,6 +1,21 @@
 $(function(){
+  let data = {};
+
   var namestorage=sessionStorage.getItem("user_name");
   document.getElementById("username").innerText=namestorage;
+
+  $.ajax({
+    url:"",
+    type:"GET",
+    contentType:"application/json:charset=utf-8",
+    dataType:"json",
+    success:function(result){
+      if(result.resCode="0000"){
+        console.log("result===",result)
+        console.log("result.data===",result.data)
+      }
+    }
+  })
 
 
 
