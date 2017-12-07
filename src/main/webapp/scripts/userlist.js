389dd322d779342e420c67f8c2f7de4555727726
jyf <jyf@qq.com>
2017-12-07 09:47:34 +0800

    0

--- src/main/webapp/scripts/userlist.js
+++ src/main/webapp/scripts/userlist.js
@@ -1,26 +1,98 @@
 var returndata;
 $(function(){
+  let data = {};
 
   var namestorage=sessionStorage.getItem("user_name");
   document.getElementById("username").innerText=namestorage;
 
-  getUsers();
-
+  //获取所有用户
+  $.ajax({
+    url:"/user/selectAllUser",
+    type:"GET",
+    contentType:"application/json:charset=utf-8",
+    dataType:"json",
+    success:function(result){
+      data=result.data;
+      if(result.resCode="0000"){
+        console.log("result--",result);
+        returndata=result.data;
+        showData(returndata);
+      }
+    },
+    error: function (result) {
+      console.log("数据获取出错");
+    }
+  });
+  //查询
   $("#search_id").click(function(){
       searchUser();
   });
-
+  //添加
   $("#addUser").click(function(){
       addUser();
   });
 
-  $("delUser").click(function(){
-      delUser();
-  });
-
-  $("editUser").click(function(){
-      editUser();
-  });
+  $("#userlist").on("click",function(e){
+    const name=$(e.target).attr("name");
+    const type=$(e.target).attr("type");
+    const operate=$(e.target).html();
+    console.log("e.target---",e.target);
+    if(type=="button"&&operate=="删除"){
+      //删除
+      $("#delUser").click(function(){
+        const array =[data[name].uid];
+        console.log("array---",array);
+        $.ajax({
+          url:"/user/deleteUser",
+          type:"POST",
+          data:JSON.stringify(array),
+          contentType:"application/json",
+          cache:false,
+          success:function(result){
+            console.log("result1---",result);
+            result=JSON.parse(result);
+            console.log("result2---",result);
+          },
+          error:function(result){
+            console.log("删除出错");
+          }
+        })
+      })
+    }else if (type=="button"&&operate=="编辑") {
+      let user=data[name];
+      console.log("user---",user);
+      $("#usersname").val(user.uname);
+      $("#useremail").val(user.uemail);
+      $("#userrole").val(user.role);
+      //编辑
+      $("#editUser").click(function(){
+        const form =document.getElementById("edit-form");
+        let postData={};
+        let formData=new formData(form);
+        formData.forEach((value,key)=>postData[key]=value);
+        $.ajax({
+          url:"",
+          type:"POST",
+          contentType: "application/json;charset=utf-8",
+          dataType: "json",
+          data:JSON.stringify(postData),
+          success: function (result) {
+            console.log("editresult---",result);
+            if(result.data==""){
+              alert("编辑成功");
+              window.location.reload();
+            }
+            else{
+              console.log("编辑失败");
+            }
+          },
+          error: function (result) {
+            console.log("编辑出错");
+          }
+        })
+      })
+    }
+  })
 
 })
 
@@ -44,31 +116,12 @@ function showData(returndata){
           html=html+"<tr><td>"+result[i].uname+"</td><td>"
           +result[i].uemail+"</td><td>"
           +result[i].role+
-          "</td><td><button name='" + j + "' class='btn btn-primary btn-sm' data-toggle='modal' data-target='#edit_modal'>编辑</button><button name='" + j + "' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#del_modal'>删除</button></td></tr>";
+          "</td><td><button name='" + j + "' type='button' class='btn btn-primary btn-sm' data-toggle='modal' data-target='#edit_modal'>编辑</button><button name='" + j + "' type='button' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#del_modal'>删除</button></td></tr>";
         }
         $("#userlist").html(html);
       }
     })
   }
-}
-//获取所有用户
-function getUsers(){
-  $.ajax({
-    url:"/user/selectAllUser",
-    type:"GET",
-    contentType:"application/json:charset=utf-8",
-    dataType:"json",
-    success:function(result){
-      if(result.resCode="0000"){
-        console.log("result--",result);
-        returndata=result.data;
-        showData(returndata);
-      }
-    },
-    error: function (result) {
-      console.log("数据获取出错");
-    }
-  });
 }
 //查询用户
 function searchUser(){
@@ -98,29 +151,101 @@ function addUser(){
   let postData={};
   let formData=new FormData(form);
   formData.forEach((value,key)=>postData[key]=value);
-  $.ajax({
-    url:"/user/register",
-    type:"POST",
-    async:false,
-    contentType: "application/json;charset=utf-8",
-    dataType: "json",
-    data:JSON.stringify(postData),
-    success: function (result) {
+  if($("#user_pwd").val()!=$("#confirm_pwd").val()){
+    alert("两次密码不一致");
+  }else{
+    $.ajax({
+      url:"/user/register",
+      type:"POST",
+      contentType: "application/json;charset=utf-8",
+      dataType: "json",
+      data:JSON.stringify(postData),
+      success: function (result) {
+        console.log("addresult---",result);
+        if(result.data=="新建成功"){
+          alert("新建成功");
+          window.location.reload();
+        }
+        else if(result.data=="用户名已存在"){
+          alert("用户名已存在，请重新输入");
+          window.location.reload();
+        }
+        else{
+          console.log("新建失败");
+        }
+      },
+      error: function (result) {
+        console.log("新建出错");
+      }
+    })
+  }
+}
 
-    },
-    error: function (result) {
-      console.log("添加出错");
+var uname=false;
+var pwd=false;
+var repwd=false;
+var email=false;
+//检查用户名
+function checkUname(name) {
+    uname = false;
+    if (name == "") {
+        $("#uname-tip").html("不能为空");
     }
-  })
+    else{
+        $("#uname-tip").html("");
+        uname = true;
+    }
+    button();
 }
-//删除用户
-function delUser(){
-
+//检查用户密码
+function checkPwd(name) {
+    pwd = false;
+    if (name == "") {
+        $("#upwd-tip").html("不能为空");
+    }
+    else if (name.length>=3 && name.length<=6) {
+        $("#upwd-tip").html("");
+        pwd = true;
+    } else {
+        $("#upwd-tip").html("请输入3-6位密码");
+    }
+    button();
 }
-//编辑用户
-function editUser(){
-
+//检查用户确认密码
+function checkRpwd(name) {
+    repwd = false;
+    if (name == "") {
+        $("#uconfirm-tip").html("不能为空");
+    }
+    else{
+        $("#uconfirm-tip").html("");
+        repwd = true;
+    }
+    button();
 }
+//检查邮箱
+function checkEmail(name) {
+  email = false;
+  if (name == "") {
+    $("#email-tip").html("不能为空");
+    $("#email-tip2").html("不能为空");
+  } else if (name.match(/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+\.([a-zA-Z0-9_-])+$/)) {
+    $("#email-tip").html("");
+    $("#email-tip2").html("");
+    email = true;
+  } else {
+    $("#email-tip").html("邮箱不符合规范");
+    $("#email-tip2").html("邮箱不符合规范");
+  }
+  button();
+}
+function button() {
+     var stamp = document.getElementById("addUser");
+     stamp.disabled = true;
+     if(uname && pwd && repwd && email) {
+         stamp.disabled = false;
+     }
+ }
 //回车搜索
 function keySearch(){
     if (event.keyCode == 13) {
